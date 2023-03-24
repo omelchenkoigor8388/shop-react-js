c9b32e96864b98ae7d6dc10837f07800cf3fb0f7
omelchenkoigor8388 <omelchenkoigor8388@gmail.com>
2023-03-24 18:14:18 +0200

    commit-react js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>House Staff</span>
+            <ul className='nav'>
+                <li>Про нас</li>
+                <li>Контакти</li>
+                <li>Кабінет</li>
+            </ul>
+        </div>
+        <div className='presentation'>
+
+        </div>
+    </header>
+  )
+}
